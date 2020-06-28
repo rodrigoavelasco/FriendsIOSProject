6a7c32e89ff194e4b193a01130b3b88ade49db63
rodrigoavelasco <rodrigovelasco9@gmail.com>
2020-06-27 20:08:10 -0500

    Added barebones of the project

---
+++ Friends/Friends/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by Rodrigo Velasco on 6/27/20.
+//  Copyright © 2020 Rodrigo Velasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logInButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        customizeLogInButton()
+    }
+
+    func customizeLogInButton(){
+        logInButton.setTitleColor(.white, for: .normal)
+        logInButton.backgroundColor = UIColor.blue
+        logInButton.layer.cornerRadius = 10
+        logInButton.layer.borderWidth = 1.0
+        logInButton.layer.borderColor = UIColor.darkGray.cgColor
+        
+    }
+
+}
+
